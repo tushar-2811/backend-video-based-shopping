3f5c7695378e56aa5c8925f7c1b41abc6fdf019e
Tushar Rawat <tusharrawat52@gmail.com>
2024-05-16 20:38:30 +0530

    add playlist controllers

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,6 +3,7 @@ import { Playlist } from "../models/playlist.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+
 // create a playlist
 export const createPlaylist = asyncHandler(async(req , res) => {
     const {name , description } = req.body;
@@ -38,11 +39,27 @@ export const createPlaylist = asyncHandler(async(req , res) => {
 // get user playlist
 export const getUserPlaylist = asyncHandler(async(req , res) => {
     const {userId} = req.params;
+
+    
 })
 
 // get playlist by id
 export const getPlaylist = asyncHandler(async(req , res) => {
     const {PlaylistId} = req.params;
+
+    if(!PlaylistId) {
+        throw new ApiError(400 , "Playlist Id is missing");
+    }
+
+    const existingPlaylist = await Playlist.findById(PlaylistId);
+
+    if(!existingPlaylist) {
+        throw new ApiError(500 , "Playlist not found");
+    }
+
+    return res.status(201).json(
+        new ApiResponse(201 , existingPlaylist , "Playlist found")
+    )
 
 })
 
@@ -50,6 +67,20 @@ export const getPlaylist = asyncHandler(async(req , res) => {
 // add videos to playlist
 export const addVideoToPlaylist = asyncHandler(async(req , res) => {
     const {PlaylistId , videoId} = req.params;
+
+    if(!PlaylistId) {
+        throw new ApiError(400 , "Select a valid playlist");
+    }
+
+    if(!videoId) {
+        throw new ApiError(400 , "Select a valid video to add");
+    }
+
+    const existingPlaylist = await Playlist.findByIdAndUpdate(PlaylistId , {
+        $set : {
+            
+        }
+    })
 })
 
 // remove video from playlist
@@ -61,4 +92,18 @@ export const removeVideoFromPlaylist = asyncHandler(async(req , res) => {
 // delete playlist
 export const deletePlaylist = asyncHandler(async(req , res) => {
     const {PlaylistId} = req.params;
+
+    if(!PlaylistId){
+        throw new ApiError(400 , "Playlist Id is missing");
+    }
+
+    const deletedPlaylist = await Playlist.findByIdAndDelete(PlaylistId);
+
+    if(!deletedPlaylist) {
+        throw new ApiError(500 , "Error while deleting the playlist");
+    }
+
+    return res.status(201).json(
+        new ApiResponse(201, {} , "Playlist deleted")
+    )
 })
--- src/routes/api/v1/playlist.route.js
+++ src/routes/api/v1/playlist.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { verifyJWT } from "../../../middlewares/auth.middleware.js";
-import { createPlaylist } from "../../../controllers/playlist.controller.js";
+import { createPlaylist, deletePlaylist, getPlaylist } from "../../../controllers/playlist.controller.js";
 
 const playlistRouter = Router();
 
 playlistRouter.route('/createPlaylist').post(verifyJWT , createPlaylist);
+playlistRouter.route('/getplaylist/:PlaylistId').get(getPlaylist);
+playlistRouter.route('/deleteplaylist/:PlaylistId').delete(deletePlaylist);
 
 export default playlistRouter;
